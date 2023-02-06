13bcd8629abf6fc4a5839e3189a5bf60557e28a6
Purgenta <onlinenidza352@gmail.com>
2023-02-06 21:00:09 +0100

    Added pagination and sorting along with search by title name

---
+++ src/main/java/com/purgenta/gameshop/controllers/AnnouncementsController.java
@@ -0,0 +1,2 @@
+package com.purgenta.gameshop.controllers;public class AnnouncementsController {
+}
--- src/main/java/com/purgenta/gameshop/dto/GameDto.java
+++ src/main/java/com/purgenta/gameshop/dto/GameDto.java
@@ -1,28 +1,2 @@
-package com.purgenta.gameshop.dto;
-
-import com.purgenta.gameshop.validation.game.ValidateUniqueTitle;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Data;
-
-@Data
-public class GameDto {
-    @NotNull
-    @ValidateUniqueTitle
-    private String title;
-    @NotNull
-    @Size(min = 15,max = 40)
-    private String description;
-
-    @NotNull
-    private int publisherId;
-
-    @NotNull
-    private int categoryId;
-    @Min(1950)
-    private int releaseYear;
-    @NotNull
-    @Min(1)
-    private double price;
+package com.purgenta.gameshop.dto;public class GameDTO {
 }
